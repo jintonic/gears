9e908755e1473828537506e39c7556e02d6dc1a6
Jing LIU <jing.liu@usd.edu>
2017-03-12 07:14:13 -0500

    enabled radioactive decay by default

--- gears.C
+++ gears.C
@@ -498,33 +498,30 @@ class Physics: public G4VModularPhysicsList, public G4UImessenger
 };
 //______________________________________________________________________________
 //
-#include "G4EmStandardPhysics.hh"
 #include "G4DecayPhysics.hh"
+#include "G4RadioactiveDecayPhysics.hh"
+#include "G4EmStandardPhysics.hh"
 Physics::Physics() : G4VModularPhysicsList(), G4UImessenger()
 {
    fCmd = new G4UIcmdWithAString("/physics_lists/enable",this);
    fCmd->SetGuidance("Enable a certain physics list");
    fCmd->SetParameterName("name of a physics list", false);
-   fCmd->SetCandidates("RadioactiveDecay Optical HadronElastic HadronInelastic");
+   fCmd->SetCandidates("Optical HadronElastic HadronInelastic");
    fCmd->AvailableForStates(G4State_PreInit);
 
-   RegisterPhysics(new G4EmStandardPhysics());
-   // has to be loaded to define some particles for hadron processes:
-   RegisterPhysics(new G4DecayPhysics()); // muon, pion, kaon etc.
+   RegisterPhysics(new G4DecayPhysics()); // defined many particles
+   RegisterPhysics(new G4RadioactiveDecayPhysics());
+   RegisterPhysics(new G4EmStandardPhysics()); // must be after RadioactiveDecay
 }
 //______________________________________________________________________________
 //
 #include "G4OpticalPhysics.hh"
-#include "G4RadioactiveDecayPhysics.hh"
 #include "G4HadronElasticPhysicsHP.hh"
 #include "G4HadronPhysicsFTFP_BERT_HP.hh" // won't work for Geant4 version<10
 void Physics::SetNewValue(G4UIcommand* cmd, G4String value)
 {
    if (cmd==fCmd) {
-      if (value=="RadioactiveDecay") // alpha, beta, gamma, etc.
-         RegisterPhysics(new G4RadioactiveDecayPhysics());
-      else if (value=="Optical")
-         RegisterPhysics(new G4OpticalPhysics());
+      if (value=="Optical") RegisterPhysics(new G4OpticalPhysics());
       else if (value=="HadronElastic")
          RegisterPhysics(new G4HadronElasticPhysicsHP());
       else if (value=="HadronInelastic")
