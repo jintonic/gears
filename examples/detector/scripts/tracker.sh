9cfc373c29f067575e7b863a29ec065a36fa72a0
Jing LIU <jing.liu@usd.edu>
2020-03-03 06:38:48 -0600

    added

---
+++ examples/detector/scripts/tracker.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+output="tracker.tg"
+if [ $# -eq 2 ]; then output=$1; fi
+
+cat << EOC > $output
+:volu world BOX 10*cm 10*cm 10*cm G4_Galactic
+:vis world OFF
+
+:rotm r000 0 0 0
+
+:volu tracker BOX 1*micrometer 9*cm 9*cm G4_Galactic
+EOC
+
+id=1
+while [ $id -le 81 ]; do
+  x=$((id*2-82))*mm
+  echo ":place tracker $id world r000 $x 0 0" >> $output
+  echo ":color tracker 0.3 0.4 0.3" >> $output
+  id=$((id+1))
+done
