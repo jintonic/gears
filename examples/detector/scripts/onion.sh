31e5e164c22bd9f45e7e48d12b38b7cc70147bf7
Jing LIU <jing.liu@usd.edu>
2020-03-05 23:41:45 -0600

    reduced number of skins

--- examples/detector/scripts/onion.sh
+++ examples/detector/scripts/onion.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 output="onion.tg"
+echo -n "Creating $output... "
 
 cat << END > $output
 // default unit for length is mm
-:volu skin100 ORB 100 G4_AIR
-:color skin100 0.2 0.4 0.3
+:volu skin50 ORB 50 G4_AIR
+:color skin50 0.5 0 0.5
 
 :rotm r000 0 0 0
 
 END
 
-r=95
+r=45
 while [ $r -ge 5 ]; do
   echo ":volu skin$r ORB $r G4_AIR" >> $output
   echo ":place skin$r 1 skin$((r+5)) r000 0 0 0" >> $output
-  echo ":color skin$r 0.3 0.4 0.$r" >> $output
+  echo ":color skin$r 0.3 0 0.$r" >> $output
   r=$((r-5))
 done
+echo Done.
