52fc068bcff83d1122bd68e7dfcfb1897aec0d7c
Jing LIU <jing.liu@usd.edu>
2020-04-22 21:50:28 -0500

    added

---
+++ install/geant4.bat
@@ -0,0 +1,15 @@
+:: http://geant4-userdoc.web.cern.ch/geant4-userdoc/UsersGuides/InstallationGuide/html/postinstall.html#required-environment-settings-on-windows
+:: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/setx
+setx G4LEDATA "C:\Program Files\Geant4 10.6\share\Geant4-10.6.1\data\G4EMLOW7.9.1"
+setx G4LEVELGAMMADATA "C:\Program Files\Geant4 10.6\share\Geant4-10.6.1\data\PhotonEvaporation5.5"
+setx G4NEUTRONHPDATA "C:\Program Files\Geant4 10.6\share\Geant4-10.6.1\data\G4NDL4.6"
+setx G4ENSDFSTATEDATA "C:\Program Files\Geant4 10.6\share\Geant4-10.6.1\data\G4ENSDFSTATE2.2"
+setx G4RADIOACTIVEDATA "C:\Program Files\Geant4 10.6\share\Geant4-10.6.1\data\RadioactiveDecay5.4"
+setx G4ABLADATA "C:\Program Files\Geant4 10.6\share\Geant4-10.6.1\data\G4ABLA3.1"
+setx G4PIIDATA "C:\Program Files\Geant4 10.6\share\Geant4-10.6.1\data\G4PII1.3"
+setx G4PARTICLEXSDATA "C:\Program Files\Geant4 10.6\share\Geant4-10.6.1\data\G4PARTICLEXS2.1"
+setx G4SAIDXSDATA "C:\Program Files\Geant4 10.6\share\Geant4-10.6.1\data\G4SAIDDATA2.0"
+setx G4REALSURFACEDATA "C:\Program Files\Geant4 10.6\share\Geant4-10.6.1\data\RealSurface2.1.1"
+setx G4INCLDATA "C:\Program Files\Geant4 10.6\share\Geant4-10.6.1\data\G4INCL1.0"
+setx G4UI_USE_WIN32 1
+@pause
