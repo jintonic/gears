48785cad1e50b0edad2710f60356f3348b2b5684
Jing LIU <jing.liu@usd.edu>
2020-04-26 11:12:36 -0500

    scanned directory

--- install/geant4.bat
+++ install/geant4.bat
@@ -1,15 +1,84 @@
+:: scan Geant4 data directories in current folder
+:: and set environment variables accordingly
+:: References:
+:: https://ss64.com/nt/for.html
 :: http://geant4-userdoc.web.cern.ch/geant4-userdoc/UsersGuides/InstallationGuide/html/postinstall.html#required-environment-settings-on-windows
 :: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/setx
-setx G4LEDATA "C:\Program Files\Geant4 10.6\share\Geant4-10.6.1\data\G4EMLOW7.9.1"
-setx G4LEVELGAMMADATA "C:\Program Files\Geant4 10.6\share\Geant4-10.6.1\data\PhotonEvaporation5.5"
-setx G4NEUTRONHPDATA "C:\Program Files\Geant4 10.6\share\Geant4-10.6.1\data\G4NDL4.6"
-setx G4ENSDFSTATEDATA "C:\Program Files\Geant4 10.6\share\Geant4-10.6.1\data\G4ENSDFSTATE2.2"
-setx G4RADIOACTIVEDATA "C:\Program Files\Geant4 10.6\share\Geant4-10.6.1\data\RadioactiveDecay5.4"
-setx G4ABLADATA "C:\Program Files\Geant4 10.6\share\Geant4-10.6.1\data\G4ABLA3.1"
-setx G4PIIDATA "C:\Program Files\Geant4 10.6\share\Geant4-10.6.1\data\G4PII1.3"
-setx G4PARTICLEXSDATA "C:\Program Files\Geant4 10.6\share\Geant4-10.6.1\data\G4PARTICLEXS2.1"
-setx G4SAIDXSDATA "C:\Program Files\Geant4 10.6\share\Geant4-10.6.1\data\G4SAIDDATA2.0"
-setx G4REALSURFACEDATA "C:\Program Files\Geant4 10.6\share\Geant4-10.6.1\data\RealSurface2.1.1"
-setx G4INCLDATA "C:\Program Files\Geant4 10.6\share\Geant4-10.6.1\data\G4INCL1.0"
+:: https://stackoverflow.com/questions/4419868/what-is-the-current-directory-in-a-batch-file
+:: https://stackoverflow.com/questions/1501151/windows-bat-to-move-all-directories-matching-mask-from-dir-a-to-dir-b
+:: https://stackoverflow.com/questions/252286/how-do-i-perform-string-operations-on-variables-in-a-for-loop/6310580#6310580
+:: https://stackoverflow.com/questions/14347038/why-are-my-set-commands-resulting-in-nothing-getting-stored/14347131#14347131
+@echo OFF
+
 setx G4UI_USE_WIN32 1
-@pause
+
+setlocal enabledelayedexpansion
+for /D %%i in ("*") do (
+  set dir=%%i
+  set dir=!dir:G4ABLA=!
+  if not %%i==!dir! (
+    echo setx G4ABLADATA "%~dp0%%i"
+    setx G4ABLADATA "%~dp0%%i"
+  )
+  set dir=%%i
+  set dir=!dir:G4ENSDFSTATE=!
+  if not %%i==!dir! (
+    echo setx G4ENSDFSTATEDATA "%~dp0%%i"
+    setx G4ENSDFSTATEDATA "%~dp0%%i"
+  )
+  set dir=%%i
+  set dir=!dir:G4INCL=!
+  if not %%i==!dir! (
+    echo setx G4INCLDATA "%~dp0%%i"
+    setx G4INCLDATA "%~dp0%%i"
+  )
+  set dir=%%i
+  set dir=!dir:G4EMLOW=!
+  if not %%i==!dir! (
+    echo setx G4LEDATA "%~dp0%%i"
+    setx G4LEDATA "%~dp0%%i"
+  )
+  set dir=%%i
+  set dir=!dir:PhotonEvaporation=!
+  if not %%i==!dir! (
+    echo setx G4LEVELGAMMADATA "%~dp0%%i"
+    setx G4LEVELGAMMADATA "%~dp0%%i"
+  )
+  set dir=%%i
+  set dir=!dir:G4NDL=!
+  if not %%i==!dir! (
+    echo setx G4NEUTRONHPDATA "%~dp0%%i"
+    setx G4NEUTRONHPDATA "%~dp0%%i"
+  )
+  set dir=%%i
+  set dir=!dir:G4PARTICLEXS=!
+  if not %%i==!dir! (
+    echo setx G4PARTICLEXSDATA "%~dp0%%i"
+    setx G4PARTICLEXSDATA "%~dp0%%i"
+  )
+  set dir=%%i
+  set dir=!dir:G4PII=!
+  if not %%i==!dir! (
+    echo setx G4PIIDATA "%~dp0%%i"
+    setx G4PIIDATA "%~dp0%%i"
+  )
+  set dir=%%i
+  set dir=!dir:RadioactiveDecay=!
+  if not %%i==!dir! (
+    echo setx G4RADIOACTIVEDATA "%~dp0%%i"
+    setx G4RADIOACTIVEDATA "%~dp0%%i"
+  )
+  set dir=%%i
+  set dir=!dir:RealSurface=!
+  if not %%i==!dir! (
+    echo setx G4REALSURFACEDATA "%~dp0%%i"
+    setx G4REALSURFACEDATA "%~dp0%%i"
+  )
+  set dir=%%i
+  set dir=!dir:G4SAIDDATA=!
+  if not %%i==!dir! (
+    echo setx G4SAIDXSDATA "%~dp0%%i"
+    setx G4SAIDXSDATA "%~dp0%%i"
+  )
+)
+pause
