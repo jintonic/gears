d00ca6fdb028a5d82c63575485774910d864c9f8
Jing LIU <jing.liu@usd.edu>
2021-01-09 07:25:34 -0600

    added more comments

--- gears.bat
+++ gears.bat
@@ -1,15 +1,15 @@
 :: Add gears executable folder to user PATH
-:: References:
-:: https://ss64.com/nt/for.html
-:: https://stackoverflow.com/questions/14509652/what-is-the-difference-between-and-in-a-cmd-file
-:: https://stackoverflow.com/questions/46712814/get-current-users-path-variable-without-system-path-using-cmd
 
+:: https://ss64.com/nt/for.html
 @echo OFF
 
+:: https://stackoverflow.com/questions/46712814
+:: https://stackoverflow.com/questions/14509652
 for /f "skip=2 tokens=1-2*" %%G in ('Reg Query HKCU\Environment /V PATH 2^>Nul') do set user_path=%%I
 
-set new_path=%user_path:gears=%
-if "%new_path%"=="%user_path%" (
+:: https://stackoverflow.com/questions/2772456
+:: https://stackoverflow.com/questions/4419868
+if "%user_path:gears=%"=="%user_path%" (
   echo Add %~dp0 to user PATH
   setx path %user_path%%~dp0
 ) else (
