c5ea75fca37be6cd30a456a6c762fe19cd52e991
Jing LIU <jing.liu@usd.edu>
2022-08-18 00:19:39 -0500

    installed datasets in physino/geant4:gears

--- INSTALL/Geant4/entrypoint.sh
+++ INSTALL/Geant4/entrypoint.sh
@@ -6,8 +6,4 @@
 # ENV PS1="\u@Geant4:\w \$ " doesn't work together with ENTRYPOINT
 export PS1="\u@Geant4:\w \$ "
 
-if [ ! -d /root/gears/INSTALL/Geant4/data/G4ENSDFSTATE* ]; then
-  geant4-config --install-datasets
-fi
-
 exec "$@" # run cmd specified in Dockerfile or docker-compose.yml
