cab3db070836f35e406c0ee8069b59de422f79a2
Jing Liu <jintonic@physino.xyz>
2023-01-26 00:10:19 -0600

    removed

--- INSTALL/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# use geant4.sh to avoid hard coding datasets' locaitons
-. /usr/bin/geant4.sh
-
-# ENV PS1="\u@Geant4:\w \$ " doesn't work together with ENTRYPOINT
-export PS1="\u@Geant4:\w \$ "
-
-exec "$@" # run cmd specified in Dockerfile or docker-compose.yml
