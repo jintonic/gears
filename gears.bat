887d20e78591fcc34f8ccc5822f232e8eec24ef7
Jing Liu <jintonic@physino.xyz>
2023-02-07 11:54:45 -0600

    simplified a bit

--- gears.bat
+++ gears.bat
@@ -1,23 +1,19 @@
 :: Add gears executable folder to user PATH
 
-:: https://ss64.com/nt/for.html
+:: Disable printing commands on screen
 @echo OFF
 
 if not exist gears.exe (
-  echo cannot find gears.exe, please compile it first
+  echo cannot find gears.exe, please compile gears.cc first
   pause & exit /b
 )
 
+:: Get curernt user PATH from the registry
 :: https://stackoverflow.com/questions/46712814
 :: https://stackoverflow.com/questions/14509652
-for /f "skip=2 tokens=1-2*" %%G in ('Reg Query HKCU\Environment /V PATH 2^>Nul') do set user_path=%%I
+for /f "skip=2 tokens=1-2*" %%G in ('Reg Query HKCU\Environment /V PATH 2^>nul') do set user_path=%%I
 
-:: https://stackoverflow.com/questions/2772456
-:: https://stackoverflow.com/questions/4419868
-if "%user_path:gears=%"=="%user_path%" (
-  echo Add %~dp0 to user PATH
-  setx path %user_path%%~dp0
-) else (
-  echo It seems %~dp0 is already in PATH:
-  echo %user_path%
-)
+echo add "%~dp0" to user PATH
+:: setx won't create duplicated entries,
+:: the following command can be run multiple times
+setx path "%user_path%%~dp0"
