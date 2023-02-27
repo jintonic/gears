37abdb27268e8192ffe87e18fe358002b66e61b9
Jing Liu <jintonic@physino.xyz>
2023-02-26 22:18:44 -0600

    enabled color prompt

--- INSTALL/Geant4/Docker/entrypoint.sh
+++ INSTALL/Geant4/Docker/entrypoint.sh
@@ -4,6 +4,7 @@
 . /usr/bin/geant4.sh
 
 # ENV PS1="\u@Geant4:\w \$ " doesn't work together with ENTRYPOINT
-export PS1="\u@Geant4:\w \$ "
+# https://superuser.com/a/367280
+export PS1="\[\e[0;32m\]\u@Geant4:\[\e[0;34m\]\w \[\e[0;31m\]\$\[\e[m\] "
 
 exec "$@" # run cmd specified in Dockerfile or docker-compose.yml
