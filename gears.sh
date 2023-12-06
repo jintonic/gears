cfc55b7885e12cdcd9bcad9bfad1ffb5acd2ef1e
Jing Liu <jintonic@physino.xyz>
2023-12-05 22:47:46 -0600

    check if geant4.sh is sourced

--- gears.sh
+++ gears.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# quit if geant4-config cannot be found
+if ! hash geant4-config 2>/dev/null; then
+  echo Do not forget to source geant4.sh; exit
+fi
+
 # add gears directory to PATH
 export GEARS="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 export PATH=$GEARS/tutorials/detector/visualization:$GEARS:$PATH
