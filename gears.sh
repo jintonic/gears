af8e8b3b7353a961448ae974fcd144f0e9c14bd4
Jing Liu <jintonic@physino.xyz>
2023-12-12 22:42:16 -0600

    fixed cmd to allocate gears.sh's location

--- gears.sh
+++ gears.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # add gears directory to PATH
-GEARS="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+GEARS="$(cd "$(dirname "${BASH_SOURCE[0]:-$0}")" >/dev/null 2>&1 && pwd)"
 export PATH=$GEARS:$PATH
