b6ad37cbcf14f614dfaf91a70a2016b630d4935e
Jing Liu <jintonic@physino.xyz>
2023-12-12 22:36:00 -0600

    removed visualization path from gears.sh

--- gears.sh
+++ gears.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # add gears directory to PATH
 GEARS="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-export PATH=$GEARS/tutorials/detector/visualization:$GEARS:$PATH
+export PATH=$GEARS:$PATH
