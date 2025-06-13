05ae22aaddcd12ddee9a4911f9aefc888b885f00
Jing Liu <jintonic@physino.xyz>
2025-06-13 14:42:32 -0500

    abort if surface finish is wrongly specified

--- gears.cc
+++ gears.cc
@@ -257,7 +257,10 @@ G4bool LineProcessor::ProcessLine(const vector<G4String> &words)
           bdr->optic->SetFinish(groundfrontpainted);
         else if (value=="groundbackpainted")
           bdr->optic->SetFinish(groundbackpainted);
-        else G4cout<<"GERAS: Ignore unknown surface finish "<<value<<G4endl;
+        else {
+          G4cout<<"GERAS: Unknown surface finish: "<<value<<"!"<<G4endl;
+          abort();
+        }
       } else if (setting=="sigma_alpha") {
         bdr->optic->SetSigmaAlpha(G4UIcommand::ConvertToInt(value));
       } else
