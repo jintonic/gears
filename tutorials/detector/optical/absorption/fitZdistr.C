277a35b287e50ae94084a4964829849250c498f5
Jing Liu <jintonic@physino.xyz>
2025-12-01 07:04:50 -0600

    added

---
+++ tutorials/detector/optical/absorption/fitZdistr.C
@@ -0,0 +1,28 @@
+{
+	gStyle->SetOptFit(0);
+	gStyle->SetOptStat(0);
+	gStyle->SetPadTopMargin(0.01);
+	gStyle->SetPadLeftMargin(0.11);
+	gStyle->SetPadRightMargin(0.01);
+	TChain t("t");
+	t.Add("absorption.root");
+
+	TH1F* h = new TH1F("h", ";Penetration depth [m]; Entries", 100, -3.5, 0);
+	t.Draw("z/1000>>h", "pdg==-22 && pro==3031");
+
+	h->Fit("expo");
+	TF1* f = h->GetFunction("expo");
+	f->SetLineColor(kRed);
+
+	TLatex l1, l2;
+	l1.SetTextFont(22); l1.SetTextSize(0.06); l1.SetTextColor(kRed);
+	l2.SetTextFont(22); l2.SetTextSize(0.06);
+	l1.DrawLatex(-1.1, 800, "f(z)=A e^{z/#tau}");
+	l2.DrawLatex(-1.1, 500, Form("#tau = %.3f #pm 0.004", 1/f->GetParameter(1)));
+
+	TImage *p = TImage::Open("depth.png");
+	TPad *l = new TPad("l","l",0.2,0.5,0.7,0.9);
+	l->Draw();
+	l->cd();
+	p->Draw();
+}
